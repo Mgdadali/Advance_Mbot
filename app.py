6252f39ac324047735e79cd82cf4c5cbb73d86dd
Mgdadali <109249013+Mgdadali@users.noreply.github.com>
2025-06-16 03:57:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 import os
 import json
-import time
-import requests
 import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
@@ -16,10 +14,13 @@ SERVICE_ACCOUNT_FILE = 'refined-lotus-389521-b4d8a307552e.json'
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 credentials = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=scopes)
 client = gspread.authorize(credentials)
-sheet = client.worksheet("الورقة 1")
+sheet = client.open_by_key(SHEET_ID).worksheet("الورقة1")
 
 # قائمة الموظفين (أرقام الواتساب)
-EMPLOYEES = ["201029664170", "201029773000", "201029772000", "201055855040", "201029455000", "201027480870", "201055855030"]
+EMPLOYEES = [
+    "201029664170", "201029773000", "201029772000",
+    "201055855040", "201029455000", "201027480870", "201055855030"
+]
 
 def assign_employee():
     data = sheet.get_all_records()
@@ -39,10 +40,6 @@ def save_client(phone, message):
     assigned_to = assign_employee()
     sheet.append_row([phone, assigned_to, message, now])
     return assigned_to
-
-# إعدادات Ultramsg
-ULTRAMSG_INSTANCE_ID = 'instance124923'
-ULTRAMSG_TOKEN = 'cy1phhf1mrsg8eia'
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -64,27 +61,8 @@ def webhook():
     if is_existing_client(sender):
         return "Already assigned", 200
 
-    assigned_to = save_client(sender, msg_body)
-    link = f"https://api.whatsapp.com/send?phone=+{assigned_to}"
-
-    body = (
-        "مرحب بيك  في تو فور ناين للاستشارات التعليمية والتدريب!\n\n"
-        "سعداء بتواصلك معانا، وعشان نقدر نساعدك بصورة أدق وأسرع، حنحوّلك للمستشار المختص.\n\n"
-        f"👇 اضغط هنا وتواصل معاه مباشرة:\n{link}"
-    )
-
-    response = requests.post(
-        f"https://api.ultramsg.com/{ULTRAMSG_INSTANCE_ID}/messages/chat",
-        headers={"Content-Type": "application/json"},
-        json={
-            "token": ULTRAMSG_TOKEN,
-            "to": sender,
-            "body": body
-        }
-    )
-
-    print("Ultramsg Response:", response.status_code, response.text)
-    return "Message sent", 200
+    save_client(sender, msg_body)
+    return "Logged", 200
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
